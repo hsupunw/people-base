7d2508e515acf0cca61f7dc7e49c21c68b677b3d
Supun <hsupunw@gmail.com>
2020-01-18 22:39:20 +0530

    change data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO PERSON (ID, CREATED, MODIFIED, FIRST_NAME, LAST_NAME, AGE, FAVOURITE_COLOUR, HOBBY) VALUES
-  ('123', Instant.now(), Instant.now(), 'Supun', 'Wijerathne', 28, 'Yellow', 'movies;sports' );
+  ('123', Instant.now(), Instant.now(), 'John', 'Keynes', 29, 'red', 'shopping;football' );
